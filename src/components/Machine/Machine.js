ef7f0a80cb1bc4311ad05f913a65e65868588a66
Alex <alexander.n.scott@gmail.com>
2020-08-11 15:02:44 -0400

    added updating

--- src/components/Machine/Machine.js
+++ src/components/Machine/Machine.js
@@ -15,6 +15,28 @@ class Machine extends React.Component {
         }
     }
 
+    update = () => {
+        fetch(`http://localhost:8000/api/test/multiple/${this.state.hostname}`, {
+            method: "GET"
+        })
+        .then(response => response.json())
+        .then(result => {
+            this.setState({hostname: result[0].hostname, running: result[0].running, pDRCAIteration: result[0].pDRCAIteration, pDRCALeader: result[0].pDRCALeader, MDRLevel: result[0].MDRLevel, NeighborState: result[0].NeighborState})
+        })
+
+        console.log(this.state)
+    }
+
+    componentWillMount() {
+        this.interval = setInterval(() => {
+            this.update()
+        }, 2000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
     render() {
         return (
             <div id="singleMachine">
